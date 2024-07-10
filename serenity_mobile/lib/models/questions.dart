64eaa66e312bf0c8a41b9b39bee7d54241ccb59d
Airweekz <mamolang02@gmail.com>
2024-07-10 19:22:08 +0800

    added questionnaire & fixed small things

---
+++ serenity_mobile/lib/models/questions.dart
@@ -0,0 +1,6 @@
+class Questions {
+  final String questions;
+  final List<String> answers;
+
+  Questions({required this.questions, required this.answers});
+}
--- serenity_mobile/lib/resources/colors.dart
+++ serenity_mobile/lib/resources/colors.dart
@@ -4,4 +4,8 @@ class AppColors {
   static const darkGreen = Color(0xFF012201);
   static const lightBlue = Color(0xFF38B7BF);
   static const lightGreen = Color(0xFF7FA97E);
+  static const lighterGreen = Color(0xFFE1ECE2);
+  static const blueGreen = Color(0xFF05727A);
+  static const progressBarColor = Color(0xFF6CC1B6);
+  static const dirtyWhite = Color(0xFFF6F4EE);
 }
