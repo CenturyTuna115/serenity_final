50afdab41f7a48102290173c57dae52109957ffc
Airweekz <mamolang02@gmail.com>
2024-07-15 22:44:07 +0800

    deleted unnecessary things

--- serenity_mobile/lib/main.dart
+++ serenity_mobile/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Serenity',
-      home: Emergencymode(),
+      home: Emergencymode(), //Try rani
     );
   }
 }
